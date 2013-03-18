849ee0f15cb89663e67e7275c420cfc08b2c4c9c
comsi02 <comsi02@gmail.com>
2013-03-18 18:57:21 +0900

    log & pid & index file delete

--- sphinx-2.0.6/index.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "#---------------------------- indexing ------------------------------#\n"
-./bin/indexer --config ./etc/sphinx-min.conf --print-queries deal
-echo "#---------------------------- indexing ------------------------------#\n"
--- sphinx-2.0.6/search-filter.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "#---------------------------- searching ------------------------------#\n"
-./bin/search --config ./etc/sphinx-min.conf --filter title KI
-echo "#---------------------------- searching ------------------------------#\n"
