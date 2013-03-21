1aa48b89e92cbd910df08bd629e1ee44c793675f
송영택 <comsi02@d2.tmonc.net>
2013-03-21 18:30:56 +0900

    deal search by sphinx class working...

--- tmp/deal_search.php
+++ tmp/deal_search.php
@@ -34,23 +34,57 @@ class Sphinx {
     
     function search ($args) {
 
-        if (isset($args['host'])) $this->host = $args['host'];
+        $attr = array('host','post','index','sortby','sortexpr','groupby','groupsort','distinct','limit','offset');
+        $mode = array(
+                        'any'       => SPH_MATCH_ANY,
+                        'boolean'   => SPH_MATCH_BOOLEAN,
+                        'extended'  => SPH_MATCH_EXTENDED,
+                        'extended2' => SPH_MATCH_EXTENDED2,
+                        'phrase'    => SPH_MATCH_PHRASE
+                     );
+        $rank = array(
+                        'bm25'      => SPH_RANK_BM25,
+                        'none'      => SPH_RANK_NONE,
+                        'wordcount' => SPH_RANK_WORDCOUNT,
+                        'fieldmask' => SPH_RANK_FIELDMASK,
+                        'sph04'     => SPH_RANK_SPH04
+                     );
+        
+        foreach ($attr as $a) {
+            if (isset($args[$a])) $this->$a = $args[$a];
+        }
+
+        foreach ($mode as $key => $val) {
+            if ($args[$key] == true) $this->mode = $val;
+        }
+
+        foreach ($rank as $key => $val) {
+            if ($args[$key] == true) $this->ranker = $val;
+        }
+
+        print_r($args['filter']); 
+        
+
 
         return true;
     }
 }
 
 $args = array();
-$args['host'] = 'tmon.co.kr';
+$args['host']  = '127.0.0.1';
+$args['port']  = '9312';
+$args['index'] = 'idx_deal_AV';
+$args['any']   = true;
+$args['bm25']  = true;
+$args['filter'] = array();
+array_push($args['filter'],array('catetory_srl' => 1000));
+array_push($args['filter'],array('main_deal_srl' => 123456));
 
 $sphinx = new Sphinx ();
 
 print "==================== RESULT ============================\n";
-#print_r($sphinx);
 
 print_r($sphinx->search($args));
-print "\n";
-print_r($sphinx->host);
-print "\n";
+#print_r($sphinx);
+#print_r($args);
 
-?>
