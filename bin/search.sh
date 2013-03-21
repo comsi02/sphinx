0bb3e4b590b1847e5dccb74fbd358ee63959b717
송영택 <comsi02@d2.tmonc.net>
2013-03-21 16:17:34 +0900

    sphinx-min.conf.templete 파일 추가

--- bin/search.sh
+++ bin/search.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "#---------------------------- searching ------------------------------#\n"
-~comsi02/work/sphinx/sphinx/bin/search --config ~comsi02/work/sphinx/sphinx/etc/sphinx-min.conf
+~comsi02/work/sphinx/bin/search --config ~comsi02/work/sphinx/etc/sphinx-min.conf
 echo "#---------------------------- searching ------------------------------#\n"
--- bin/stop_searchd.sh
+++ bin/stop_searchd.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "#---------------------------- searching ------------------------------#\n"
-~comsi02/work/sphinx/sphinx/bin/searchd --config ~comsi02/work/sphinx/sphinx/etc/sphinx-min.conf --stop
+~comsi02/work/sphinx/bin/searchd --config ~comsi02/work/sphinx/etc/sphinx-min.conf --stop
 echo "#---------------------------- searching ------------------------------#\n"
