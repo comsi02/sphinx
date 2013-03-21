2d191f8c94bc312265d0493e830d38b023e667fd
송영택 <comsi02@d2.tmonc.net>
2013-03-21 16:00:10 +0900

    이동한 디렉토리 및 파일 삭제

--- sphinx-2.0.6/bin/index.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "#---------------------------- indexing ------------------------------#\n"
-~comsi02/work/sphinx/sphinx/bin/indexer --config ~comsi02/work/sphinx/sphinx/etc/sphinx-min.conf --print-queries --all
-echo "#---------------------------- indexing ------------------------------#\n"
--- sphinx-2.0.6/bin/stop_searchd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "#---------------------------- searching ------------------------------#\n"
-~comsi02/work/sphinx/sphinx/bin/searchd --config ~comsi02/work/sphinx/sphinx/etc/sphinx-min.conf --stop
-echo "#---------------------------- searching ------------------------------#\n"
