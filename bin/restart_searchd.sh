0df34ce65bae0362655e7e4a6d16120c43ce89a1
송영택 <comsi02@d2.tmonc.net>
2013-03-21 16:43:03 +0900

    file rename

--- bin/restart_searchd.sh
+++ bin/restart_searchd.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo "#---------------------------- searching ------------------------------#\n"
 ~comsi02/work/sphinx/bin/searchd --config ~comsi02/work/sphinx/etc/sphinx-min.conf --stop
+sleep 1
 ~comsi02/work/sphinx/bin/searchd --config ~comsi02/work/sphinx/etc/sphinx-min.conf
 echo "#---------------------------- searching ------------------------------#\n"
